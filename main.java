cd6a820d45777cf8321cff27bfb7c2d665e65376
igor <igor.ramos.c@hotmail.com>
2022-08-23 10:24:40 -0500

    Main estructurado

--- main.java
+++ main.java
@@ -1,5 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
     public static void main (String a []) {
-        System.out.print("Holaaaa");
+        ArrayList<String> listLetra = new ArrayList<String>();
+        ingresaLetra(listLetra);
+        vocalAndConsonat(listLetra);
+    }
+
+    public static void ingresaLetra(List<String>listaLetra){
+        Scanner scanner = new Scanner(System.in);
+        listaLetra.add(scanner.nextLine());
+    }
+
+    public static void vocalAndConsonat(List<String>listaLetra){
+        //calcula y imprime por pantalla
+        System.out.print("Desde la funcion vocal"+listaLetra);
     }
 }
